dfbab50546c9c8d13355d35792f1d9b3263365db
Ismael Baldi <IsmaelBaldi16@gmail.com>
2026-07-01 21:05:27 -0300

    Agrego entrega1 con HTML y JS

---
+++ entrega1/main.js
@@ -0,0 +1,11 @@
+let nombre = prompt("¿Como te llamas?");
+let numeroUno = parseInt(prompt("Numero 1 : ","0"));
+let numeroDos = parseInt(prompt("Numero 2 : ","0"));
+
+console.log("Hola! " + nombre + ", tu numero elegido son : " + numeroUno + " " +numeroDos);
+alert("Hola! " + nombre + ", tu numero elegido son : " + numeroUno + " y " +numeroDos);
+
+let resultado = numeroUno + numeroDos;
+
+console.log("La suma entre numero 1 y numero 2 es : " + resultado);
+alert("La suma entre numero 1 y numero 2 es : " + resultado);
