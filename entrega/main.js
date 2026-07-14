ce764245b23e784f9478fe084b41c71c00ccc4cd
Ismael Baldi <IsmaelBaldi16@gmail.com>
2026-07-14 13:36:56 -0300

    Entrega 3

--- entrega/main.js
+++ entrega/main.js
@@ -1,26 +1,64 @@
-const notaMinAprobacion = 5;
-let AlumnosAprobado = "";
-let AlumnosReprobado = "";
-alert("CALIFICACION FINAL DEL CURSO.");
-let cantAlumnos = parseInt(prompt("¿Cuantos alumnos hay en el curso?"));
-
-for(let x = 0;x < cantAlumnos;x++)
+alert("INGRESAR");
+let ingresarUsuario = "";
+let ingresarContrasena = "";
+const comprobarUsuario = function(usua,pass)
 {
-    let nomAlumno = prompt("Nombre del alumno : ");
-    let notaAlumno = parseInt(prompt("Nota final de " + nomAlumno + " : "));
-    comprobarAprobacion(nomAlumno,notaAlumno);    
-}
-function comprobarAprobacion(nombre,nota)
-{
-    if(nota >= notaMinAprobacion)
-    {
-        AlumnosAprobado += nombre + "\n";
-    }
-    else
-    {
-        AlumnosReprobado += nombre + "\n"
-    }
-}
+    let usuario = "Uruguayo";
+    let contrasena = "1234";
 
-console.log("ALUMNOS APROBADOS :\n " + AlumnosAprobado);
-console.log("ALUMNOS REPOBRADOS :\n" + AlumnosReprobado);
+    if(usuario === usua && contrasena === pass)
+    {
+        return true;
+    }
+    alert("DATOS INCORRECTOS.");
+    return false;
+}
+do
+{
+    ingresarUsuario = prompt("Ingresar usuario : ");
+    ingresarContrasena = prompt("Ingresar contraseña : ");
+}while(!comprobarUsuario(ingresarUsuario,ingresarContrasena))
+
+alert("CALCULADORA");
+let numUno = parseFloat(prompt("Valor uno : " ));
+let numDos = parseFloat(prompt("Valor dos : " ));
+while(Number.isNaN(numUno) || Number.isNaN(numDos))
+{
+    alert("Numeros invalidos. Intente nuevamente!");
+    numUno = parseFloat(prompt("Valor uno : "));
+    numDos = parseFloat(prompt("Valor dos : " ));
+}
+let operador = parseInt(prompt("ELEGIR OPERACION :\n1- SUMAR\n2- RESTA\n3- MULTIPLICACION\n4- DIVISION"));
+while(Number.isNaN(operador) || operador < 1 || operador > 4)
+{
+   alert("Error al elegir operador.Opciones entre el 1 y el 4.INTENTE NUEVAMENTE")
+   operador = parseInt(prompt("ELEGIR OPERACION :\n1- SUMAR\n2- RESTA\n3- MULTIPLICACION\n4- DIVISION")); 
+}
+function operacion(valorUno,valorDos,operador)
+{
+    switch(operador)
+    {
+        case  1: return valorUno + valorDos;
+        case  2: return valorUno - valorDos;
+        case  3: return valorUno * valorDos;
+        case  4: if(valorDos == 0)
+                  {
+                    return NaN
+                  }
+                  return valorUno / valorDos;
+        default:return NaN;
+    }
+}
+const resultado = operacion(numUno,numDos,operador);
+const aumentarVeinte = (valor)=> valor * 1.20;
+if(Number.isNaN(resultado))
+{
+    alert("NO SE PUEDE DIVIDIR ENTRE 0")
+}
+else
+{
+    const simbolos =["+","-","*","/"]
+    alert(`${numUno} ${simbolos[operador-1]} ${numDos} = ${resultado}`);
+    const aumento = aumentarVeinte(resultado);
+    alert(`${resultado} aumento un 20%. Valor con el aumento : ${aumento}`)
+}
